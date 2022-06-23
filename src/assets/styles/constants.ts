c5f23dd769b3e3683b8dffc6bfe6a33550ea8f7c
Monica Dias <msumiedias@gmail.com>
2022-06-23 10:46:17 -0300

    chore: project structure built

---
+++ src/assets/styles/constants.ts
@@ -0,0 +1,13 @@
+export const constants = {
+  logoFontFamily: "Bruno Ace SC",
+  logoFontSize: "28px",
+  logoLineHeight: "140%",
+
+  headingFontFamily: "Bruno Ace SC",
+  headingFontSize: "28px",
+  headingLineHeight: "140%",
+
+  bodyFontFamily: "Bruno Ace SC",
+  bodyFontSize: "14px",
+  bodyLineHeight: "140%",
+};
